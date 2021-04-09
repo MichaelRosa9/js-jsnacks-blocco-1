6f35e1d437609fef62fd011dc83675bf4f9b2570
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-09 10:42:50 +0200

    snack-2

---
+++ jsnack-2/js/script.js
@@ -0,0 +1,8 @@
+var parola1 = prompt("inserisci parola.")
+var parola2 = prompt("inserisci seconda parola")
+
+if(parola1.length > parola2.length){
+  console.log(parola1)
+}else{
+  console.log(parola2)
+}
