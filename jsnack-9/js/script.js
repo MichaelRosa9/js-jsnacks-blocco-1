352c54f2eed449ba1e318dc3c2644f22b3bf06a2
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-13 17:36:00 +0200

    html

--- jsnack-9/js/script.js
+++ jsnack-9/js/script.js
@@ -2,9 +2,10 @@
 
 
 somma = 0;
-for(var i = 0; i < 10; i++){
+for(var i = 0; i <= 10; i++){
   somma += i;
 }
 
 var media =parseInt(somma / 10);
-console.log(media);
+
+document.getElementById("output").innerHTML = media;
