4893e7413abdd49497664a4545646cdcf8e47592
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-13 16:52:17 +0200

    while still not working

--- jsnack-8/js/script.js
+++ jsnack-8/js/script.js
@@ -1,13 +1,12 @@
 //snak 8
 
 var utenteNumero = prompt("Inserisci un numero di 4 cifre.");
-while(!utenteNumero == 0){
-  utenteNumero
-}
-if(utenteNumero.length > 4){
-  alert("Hai inserito troppe cifre. Inserisci 4 cifre.");
-}else if(utenteNumero.length < 4){
-  alert("Hai inserito poche. Inserisci 4 cifre.");
+while(utenteNumero.length !=4){
+  if(utenteNumero.length > 4){
+    alert("Hai inserito troppe cifre. Inserisci 4 cifre.");
+  }else if(utenteNumero.length < 4){
+    alert("Hai inserito poche. Inserisci 4 cifre.");
+  }
 }
 
 var split = utenteNumero.toString().split('');
