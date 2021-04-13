efd3e23473431aac5e8457d3033fe3c44f2c7f99
MichaelRosa9 <michaelrosa9@gmail.com>
2021-04-13 16:55:48 +0200

    bad fix

--- jsnack-8/js/script.js
+++ jsnack-8/js/script.js
@@ -4,8 +4,10 @@ var utenteNumero = prompt("Inserisci un numero di 4 cifre.");
 while(utenteNumero.length !=4){
   if(utenteNumero.length > 4){
     alert("Hai inserito troppe cifre. Inserisci 4 cifre.");
+    utenteNumero = prompt("Inserisci un numero di 4 cifre.");
   }else if(utenteNumero.length < 4){
     alert("Hai inserito poche. Inserisci 4 cifre.");
+    utenteNumero = prompt("Inserisci un numero di 4 cifre.");
   }
 }
 
